0a4527c436d7b7793af79e5c4280b02c6617bc62
jorge-pc-casa <jorgelosb@gmail.com>
2019-09-12 20:43:52 -0300

    update
    sobrescrevendo o retrieve, update, partial update e criando minha proprias actions

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,3 +1,4 @@
+from rest_framework.decorators import action
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 
@@ -13,9 +14,26 @@ class PontoTuristicoViewSet(ModelViewSet):
     
     def list(self, request, *args, **kwargs):
         return Response({'teste': 123})
-    
+
     def create(self, request, *args, **kwargs):
         return Response({'teste': request.data['nome']})
         # pass
     def destroy(self, request, *args, **kwargs):
+        pass
+
+    def retrieve(self, request, *args, **kwargs):
+        pass
+
+    def update(self, request, *args, **kwargs):
+        pass
+
+    def partial_update(self, request, *args, **kwargs):
+        pass
+
+    @action(methods=['get'], detail=True)
+    def denunciar(self, request, pk=None):
+        pass
+
+    @action(methods=['get'], detail=False)
+    def teste(self, request):
         pass
