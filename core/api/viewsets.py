d090698a498b45f80d3e54882c31e1537c188fbd
jorge-pc-casa <jorgelosb@gmail.com>
2019-09-15 09:38:05 -0300

    authenticação

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 
@@ -10,6 +11,7 @@ class PontoTuristicoViewSet(ModelViewSet):
     
     serializer_class = PontoTuristicoSerializer 
     filter_backends = (SearchFilter,)
+    permission_classes = (IsAuthenticated,)
     search_fields = ('username', 'descricao')
     lookup_field = 'nome'
 
